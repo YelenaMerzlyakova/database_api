c10e2f108f40cca58c7802ab7f49d3af581494b5
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-08 11:20:32 +0200

    add sql query

---
+++ tablesql.sql
@@ -0,0 +1,14 @@
+--Use create database statement
+
+--CREATE DATABASE IF NOT EXISTS Notes;          --==> a query to create a database with the name Notes
+
+
+--To store the data in a table you you create another query by using a CREATE TABLE statement.
+
+CREATE TABLE IF NOT EXISTS Notes (            --==> a query to create a table 
+    NotesID   Int(6)  NOT NULL UNIQUE,
+    Author    varchar(100) NOT NULL,
+    Title     varchar(230) NOT NULL,
+    Body      varchar(230),
+    PRIMARY KEY (Title)
+)
