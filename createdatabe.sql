83a5545f417ac4926191629b37bbf5de213bfefc
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-08 10:58:15 +0200

    sql created

---
+++ createdatabe.sql
@@ -0,0 +1,14 @@
+--Use create database statement
+
+CREATE DATABASE IF NOT EXISTS Notes;          --==> a query to create a database with the name Notes
+
+
+--To store the data in a table you you create another query by using a CREATE TABLE statement.
+
+CREATE TABLE IF NOT EXISTS Notes (            --==> a query to create a table 
+    NotesID   Int(6)  NOT NULL AUTO_INCREMENT,
+    NotesAuthor    varchar(230) NOT NULL,
+    NotesTitle     varchar(230) NOT NULL,
+    NotesBody      varchar(230),
+    PRIMARY KEY (title),
+)
