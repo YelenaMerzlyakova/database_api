5e817694fa00b53dca6e17d48feb45e2095b5ae9
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-08 14:27:28 +0200

    code conventions adjusted

--- tablesql.sql
+++ tablesql.sql
@@ -5,10 +5,10 @@
 
 --To store the data in a table you you create another query by using a CREATE TABLE statement.
 
-CREATE TABLE IF NOT EXISTS Notes (            --==> a query to create a table 
-    NotesID   Int(6)  NOT NULL UNIQUE,
-    Author    varchar(100) NOT NULL,
-    Title     varchar(230) NOT NULL,
-    Body      varchar(230),
-    PRIMARY KEY (Title)
+CREATE TABLE IF NOT EXISTS note (            --==> a query to create a table 
+    id   Int(6)  NOT NULL UNIQUE,
+    author    varchar(255) NOT NULL,
+    title     varchar(64) NOT NULL,
+    note      varchar(255),
+    PRIMARY KEY (title)
 )
