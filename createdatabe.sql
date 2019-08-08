d0d2383cd33c5859c2ac63e068fbc77372ec7cb1
Yelena Merzlyakova <49682773+YelenaMerzlyakova@users.noreply.github.com>
2019-08-08 11:01:48 +0200

    Update createdatabe.sql
    mistakes were made

--- createdatabe.sql
+++ createdatabe.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS Notes;          --==> a query to create a database
 --To store the data in a table you you create another query by using a CREATE TABLE statement.
 
 CREATE TABLE IF NOT EXISTS Notes (            --==> a query to create a table 
-    NotesID   Int(6)  NOT NULL AUTO_INCREMENT,
+    NotesID   Int(6)  NOT NULL UNIQUE,
     NotesAuthor    varchar(230) NOT NULL,
     NotesTitle     varchar(230) NOT NULL,
     NotesBody      varchar(230),
