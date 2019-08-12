8b829f3c436122d9e43c2d3bb295052fa9fcc48b
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-12 10:28:22 +0200

    validating

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+
+$servername = "localhost:3306";
+$username = "admin";
+$password = "hidden";
+$db = "note_db";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$id = filter_var($_GET['id'],FILTER_SANITIZE_NUMBER_INT);
+$title = filter_var($_GET['title'], FILTER_SANITIZE_STRING);
+$author = filter_var($_GET['author'], FILTER_SANITIZE_STRING);
+$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+
+$errors = [];
+
+
+if ($title == "") {
+    array_push($errors, "title is empty");
+}
+
+
+if ($author == "") {
+    array_push($errors, "author is empty");
+}
+
+
+if ($message == "") {
+    array_push($errors, "message is empty");
+}
+
+?>
