d9c083868891aed9e3e9892b6bd041453987a389
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-21 22:39:04 +0200

    yet another test

--- delete.php
+++ delete.php
@@ -1,6 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Request-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: Content-Type");
 
 // header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Headers: access");
