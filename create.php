d25ad403fa96f29466965b3f2124a0bb962897f1
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-22 12:24:42 +0200

    changes headers

--- create.php
+++ create.php
@@ -1,8 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
-header('Content-Type: application/json');
+header("Access-Control-Allow-Headers: Content-Type,X-Requested-With,origin, Authorization");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+header('Content-Type: application/x-www-form-urlencoded');
 header("Access-Control-Allow-Methods: Content-Type");
 
 include 'dbconnection.php';
